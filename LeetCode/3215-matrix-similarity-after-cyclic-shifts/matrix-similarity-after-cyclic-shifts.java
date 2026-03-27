14235a469589bb3132046794533520159bff346a
kartikeyvarshney12 <148205672+kartikeyvarshney12@users.noreply.github.com>
2026-03-27 10:45:16 +0530

    Time: 1 ms (99.47%) | Memory: 46.9 MB (64.00%) - LeetSync

--- LeetCode/3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ LeetCode/3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -7,13 +7,7 @@ class Solution {
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(i % 2 == 0) {
-                    //even
-                    if(mat[i][j] != mat[i][(j - k + m) % m]) return false;
-                }
-                else {
-                    if(mat[i][j] != mat[i][(j-k+m) % m]) return false;
-                }
+                if(mat[i][j] != mat[i][(j - k + m) % m]) return false;
             }
         }
         return true;
