534fd7b6c610eafa2b8c22cc0c163d91f07b0fb2
kartikeyvarshney12 <148205672+kartikeyvarshney12@users.noreply.github.com>
2026-04-02 18:25:12 +0530

    Time: 2 ms (18.94%) | Memory: 43.5 MB (70.09%) - LeetSync

--- LeetCode/75-sort-colors/sort-colors.java
+++ LeetCode/75-sort-colors/sort-colors.java
@@ -1,9 +1,8 @@
 class Solution {
     public void sortColors(int[] nums) {
-        boolean swapped;
+        boolean swapped = false;
 
         for(int i = 0; i < nums.length; i++) {
-            swapped = false;
             for(int j = 1; j < nums.length; j++) {
                 if(nums[j] < nums[j-1]) {
                     int temp = nums[j];
